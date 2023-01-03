e38fd5c490234251fc1b5bdbc79073e46931974d
lx78WyY0J5 <9j2k37st@gmail.com>
2023-01-03 19:36:55 +0100

    responsive menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,21 +1,46 @@
-function toggle_button_menu() {
-    toggle_button("menu-main-card", true);
+set_all_text();
+
+window.addEventListener("resize", set_all_text);
+
+function set_all_text() {
+    setText("div_devmode", "button_devmode", "💻");
+    setText("menu-main-card", "button_menu", "📋");
 }
 
-function toggle_button(name, inline) {
-    var a = document.getElementById(name);
-    var b = document.getElementById("button_menu");
 
-    if (a.style.display === "none") {
+function toggle_button_menu() {
+    toggle_button("menu-main-card", true, "button_menu", "📋");
+}
+
+function toggle_button_devmode() {
+    toggle_button("div_devmode", true, "button_devmode", "💻");
+}
+
+function toggle_button(name, inline, button, icon) {
+    var a = document.getElementById(name);
+    var b = document.getElementById(button);
+
+    if (window.getComputedStyle(a).getPropertyValue("display") === "none") {
         if (inline) {
             a.style.display = "inline-block";
         }
         else {
             a.style.display = "block";
         }
-        b.textContent = "Close";
+        setText(name, button, icon);
     } else {
         a.style.display = "none";
-        b.textContent = "Menu";
+        setText(name, button, icon);
+    }
+}
+
+function setText(name, button, icon) {
+    var a = document.getElementById(name);
+    var b = document.getElementById(button);
+
+    if (window.getComputedStyle(a).getPropertyValue("display") === "none") {
+        b.textContent = "👀" + icon;
+    } else {
+        b.textContent = "❌" + icon;
     }
 }
